ae27998c8720071aec888dac68e4056f273630b9
Swasti22 <swasti.22@gmail.com>
2020-08-10 15:15:16 +0530

    Delete ymlparse.py

--- ymlparse.py
+++
@@ -1,7 +0,0 @@
-import yaml
-with open('user.yml.txt','r') as file:
-    data = yaml.safe_load(file)
-    user = data['user']
-    print(user['name'])
-    for role in user['roles']:
-        print(role)
